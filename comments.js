897478b24238d24e1ab42279bb6de60aa90a9289
Matheus Lima <101226374+matheuscaldas1@users.noreply.github.com>
2023-10-31 00:25:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Data
+const comments = [
+  {
+    id: 1,
+  }
+];
